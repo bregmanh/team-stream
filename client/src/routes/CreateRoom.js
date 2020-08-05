91e7abec68d92c97f8b3d1a9ff6857c4f18b9943
aaron3993 <aaron3993@hotmail.com>
2020-08-05 18:26:06 -0400

    Added select dropdown for creating public or private rooms

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Redirect } from "react-router-dom";
 import { v1 as uuid } from "uuid";
 import './CreateRoom.css';
 
 const CreateRoom = (props) => {
+    const publicOrPrivate = useRef()
+
     const [redirect, setRedirect] = useState(null);
 
     function create() {
         const id = uuid();
         // props.history.push(`/room/${id}`);
-        setRedirect(`/room/${id}`);
+        if (publicOrPrivate.current.value === 'public' || 'private') {
+            console.log(publicOrPrivate.current.value)
+            setRedirect(`/room/${id}`);
+        }
+        // In the list of rooms component, I can access this value using props
+        // if (publicOrPrivate.current.value === 'private') {
+
+        // }
     }
     if (redirect) {
         return <Redirect to={redirect} />
@@ -19,8 +28,15 @@ const CreateRoom = (props) => {
             <h1><span className="heading-white">Watch streams together</span> <span className="heading-green">with people you care about</span></h1>
             <div className="room-container">
                 <div className="room-buttons">
-                    <button onClick={create}>Create Room</button>
-                    <button>Private Room</button>
+                    <div className="create-room">
+                        <select ref={publicOrPrivate}>
+                            <option selected disabled>Select a Room</option>
+                            <option value="public">Public</option>
+                            <option value="private">Private</option>
+                        </select>
+                        <button onClick={create}>Create Room</button>
+                    </div>
+                    <button>Join Room</button>
                 </div>
             </div>
         </div>
