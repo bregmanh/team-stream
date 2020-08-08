3d11a545c80c82005f86f0679a5928786a85fcf0
aaron3993 <aaron3993@hotmail.com>
2020-08-07 23:18:50 -0400

    Adjusted layout of RoomListItem

--- client/src/components/RoomListItem.js
+++ client/src/components/RoomListItem.js
@@ -1,29 +1,8 @@
 import React, { useState } from 'react';
 import { Redirect } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles({
-  root: {
-    height: '40%',
-    width: '30%',
-    color: 'white',
-    backgroundColor: '#0D7C83'
-  },
-  media: {
-    padding: '20%',
-    backgroundColor: 'black'
-  },
-});
+import './RoomListItem.css'
 
 export default function RoomListItem (props) {
-  const classes = useStyles();
 
   const [redirect, setRedirect] = useState(null);
 
@@ -32,27 +11,15 @@ export default function RoomListItem (props) {
   }
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-          {props.title}
-          </Typography>
-          <Typography variant="body2" component="p">
-            Viewers: 10
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button width="20%" padding="1rem" color="#10959D" onClick={() => setRedirect(`/room/${props.id}`)}>
+    <div className="room">
+      <div className="thumbnail"></div>
+      <div className="room-info">
+        <h2>{props.title}</h2>
+        <h5>Viewers: 10</h5>
+        <button className="join-room" onClick={() => setRedirect(`/room/${props.id}`)}>
           Join
-        </Button>
-      </CardActions>
-    </Card>
+        </button>
+      </div>
+    </div>
   )
 }
