cb5a918665a50080ef0a2a20fd47ccabcbffaf27
Sophie <sophiemargueritedubois@gmail.com>
2020-08-09 15:25:08 +0000

    Bug fix- timestamp

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -6,9 +6,6 @@ export default function Message(props) {
   let position;
   if (props.message.id === props.yourID) {
     // BOTH ARE UNDEFINED
-    console.log('message going to the right');
-    console.log('props.message.id', props.message.id);
-    console.log('props.yourID', props.yourID);
     position = 'right';
   } else if (props.message.username === 'TeamStreamBot') {
     position = 'middle'
@@ -20,7 +17,7 @@ export default function Message(props) {
     <div className={`message ${position}`}>
       <div className="message-header">
         <p>{props.message.username}</p> 
-        <p>{"10:00PM"}</p>
+        <p>{props.message.time}</p>
       </div>
       <p className="message-text">
         {props.message.message}
