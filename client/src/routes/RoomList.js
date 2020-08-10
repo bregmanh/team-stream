3eabec9226d969423559eb54045115f197f728f1
aaron3993 <aaron3993@hotmail.com>
2020-08-10 12:41:57 -0400

    Decreased size of leave room icon in RoomList

--- client/src/routes/RoomList.js
+++ client/src/routes/RoomList.js
@@ -26,25 +26,7 @@ export default function RoomList (props) {
   //   return roomsArray
   // }
 
-  // const roomList = props.rooms.map(room => (
-  //   <RoomListItem
-  //     key={room.key}
-  //     title={room.title}
-  //     thumbnail={room.thumbnail}
-  //     viewers={room.viewers}
-  //   />
-  // ))
-  const list = [
-      {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
-      {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
-      {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
-      {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
-      {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
-      {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
-      {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
-    ]
-  const roomList = list.map((room) => (
-
+  const roomList = props.rooms.map(room => (
     <RoomListItem
       key={room.key}
       title={room.title}
@@ -52,6 +34,24 @@ export default function RoomList (props) {
       viewers={room.viewers}
     />
   ))
+  // const list = [
+  //     {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
+  //     {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
+  //     {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
+  //     {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
+  //     {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
+  //     {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
+  //     {key: 1, title: "Chaim", thumbnail: "", viewers: 10},
+  //   ]
+  // const roomList = list.map((room) => (
+
+  //   <RoomListItem
+  //     key={room.key}
+  //     title={room.title}
+  //     thumbnail={room.thumbnail}
+  //     viewers={room.viewers}
+  //   />
+  // ))
 
   if (redirect) {
     return <Redirect to={redirect} />
@@ -61,7 +61,7 @@ export default function RoomList (props) {
     <div className="rooms-body">
       <h1 class="teamstream">TeamStream</h1>
       <ul className="rooms-list">{roomList}</ul>
-      <ExitToAppIcon className="back-icon" style={{fontSize: '6rem'}} onClick={() => setRedirect("/")}></ExitToAppIcon>
+      <ExitToAppIcon className="back-icon" style={{fontSize: '5rem'}} onClick={() => setRedirect("/")}></ExitToAppIcon>
       <p class="info">Only public rooms are displayed here. To join a private room, please request the link from the host</p>
     </div>
   )
