18a63caa5171a3208bfe535a4acb4ec8f1c60a4f
Sophie <sophiemargueritedubois@gmail.com>
2020-08-12 02:29:28 +0000

    Added media queries fo when the screen is only half size

--- client/src/components/VerticalNav.js
+++ client/src/components/VerticalNav.js
@@ -18,12 +18,12 @@ export default function VerticalNav(props) {
   return (
 
    <ul class="v-nav">
-     <li class={`${props.selection === 'leave' ? 'v-nav-icon active' : 'v-nav-icon'}`}><HomeRoundedIcon onClick={() => handleClick('leave')} fontSize="large"/></li>
-     <li class={`${props.selection === 'chat' ? 'v-nav-icon active' : 'v-nav-icon'}`}><ModeCommentOutlinedIcon onClick={() => handleClick('chat')} fontSize="large"/></li>
-     <li class={`${props.selection === 'users' ? 'v-nav-icon active' : 'v-nav-icon'}`}><PeopleAltOutlinedIcon  onClick={() => handleClick('users')} fontSize="large"/></li>
-     <li class={`${props.selection === 'newuser' ? 'v-nav-icon active' : 'v-nav-icon'}`}><PersonAddOutlinedIcon onClick={() => handleClick('newuser')} fontSize="large"/></li>
-     <li class={`${props.selection === 'queue' ? 'v-nav-icon active' : 'v-nav-icon'}`}><SubscriptionsOutlinedIcon onClick={() => handleClick('queue')} fontSize="large"/></li>
-     <li class={`${props.selection === 'addqueue' ? 'v-nav-icon active' : 'v-nav-icon'}`}><SearchOutlinedIcon onClick={() => handleClick('addqueue')} fontSize="large"/></li>
+     <li class={`${props.selection === 'leave' ? 'v-nav-icon active' : 'v-nav-icon'}`}><HomeRoundedIcon onClick={() => handleClick('leave')}/></li>
+     <li class={`${props.selection === 'chat' ? 'v-nav-icon active' : 'v-nav-icon'}`}><ModeCommentOutlinedIcon onClick={() => handleClick('chat')}/></li>
+     <li class={`${props.selection === 'users' ? 'v-nav-icon active' : 'v-nav-icon'}`}><PeopleAltOutlinedIcon  onClick={() => handleClick('users')}/></li>
+     <li class={`${props.selection === 'newuser' ? 'v-nav-icon active' : 'v-nav-icon'}`}><PersonAddOutlinedIcon onClick={() => handleClick('newuser')}/></li>
+     <li class={`${props.selection === 'queue' ? 'v-nav-icon active' : 'v-nav-icon'}`}><SubscriptionsOutlinedIcon onClick={() => handleClick('queue')}/></li>
+     <li class={`${props.selection === 'addqueue' ? 'v-nav-icon active' : 'v-nav-icon'}`}><SearchOutlinedIcon onClick={() => handleClick('addqueue')}/></li>
    </ul>
 
   )
