b62714b1cf235d2014a7939f463acd7f0d3fa75d
Sophie <sophiemargueritedubois@gmail.com>
2020-08-12 21:23:59 +0000

    changed id of seed videos

--- db/seeds/videos.js
+++ db/seeds/videos.js
@@ -4,8 +4,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('videos').insert([
-        {id: 1, thumbnail: 'https://i.ytimg.com/vi/PLOPygVcaVE/default.jpg', session_id: 1},
-        {id: 2, thumbnail: 'https://i.ytimg.com/vi/cf02KSCTT9Y/default.jpg', session_id: 2},
+        {id: 100, thumbnail: 'https://i.ytimg.com/vi/PLOPygVcaVE/default.jpg', session_id: 1},
+        {id: 101, thumbnail: 'https://i.ytimg.com/vi/cf02KSCTT9Y/default.jpg', session_id: 2},
       ]);
     });
 };
